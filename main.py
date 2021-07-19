9378bcdac4f41bbf2703fb2ce45fb71a2266e83f
chirudeepnamini <36069804+chirudeepnamini@users.noreply.github.com>
2021-07-19 18:39:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI,Path
 
 from fastapi.encoders import jsonable_encoder
 from typing import Optional
-import secrets,sqlite3
+import sqlite3
+import random
 
 
 
@@ -16,6 +17,17 @@ def data_from_db(db_name,stmt):
     return result
 
 
+# @app.get("/get-random-movie/{rating}")
+# async def fun(rating:int=Path(5,title="sth",le=9,ge=5),numVotes:int=5000):
+    
+#     db_table={5:["data5.db","ratings_gt5"],6:["data6.db","ratings_gt6"],7:["data7.db","ratings_gt7"],8:["data8.db","ratings_gt8"],9:["data9.db","ratings_gt9"]}
+#     result=data_from_db(db_table[rating][0],f"select * from {db_table[rating][1]} where numVotes>="+str(numVotes))
+#     if(len(result)==0):
+#         return {"Error": "can't find any movie,decrease numVotes"}
+#     index=secrets.randbelow(len(result))
+#     print(result[index])
+#     link="https://www.imdb.com/title/"+result[index][0]
+#     return {"link":link,"averageRating":result[index][1],"numVotes":result[index][2]}
 @app.get("/get-random-movie/{rating}")
 async def fun(rating:int=Path(5,title="sth",le=9,ge=5),numVotes:int=5000):
     
@@ -23,8 +35,7 @@ async def fun(rating:int=Path(5,title="sth",le=9,ge=5),numVotes:int=5000):
     result=data_from_db(db_table[rating][0],f"select * from {db_table[rating][1]} where numVotes>="+str(numVotes))
     if(len(result)==0):
         return {"Error": "can't find any movie,decrease numVotes"}
-    index=secrets.randbelow(len(result))
-    print(result[index])
-    link="https://www.imdb.com/title/"+result[index][0]
-    return {"link":link,"averageRating":result[index][1],"numVotes":result[index][2]}
-
+    movie=random.choice(result)
+    print(movie)
+    link="https://www.imdb.com/title/"+movie[0]
+    return {"link":link,"averageRating":movie[1],"numVotes":movie[2]}
